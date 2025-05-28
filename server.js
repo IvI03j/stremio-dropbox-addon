a617c84b9564fd678e939d90e14cc7ad432111f9
IvI03j <91011818+IvI03j@users.noreply.github.com>
2025-05-28 18:43:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+const express = require('express');
 const { addonBuilder } = require('stremio-addon-sdk');
 
 const manifest = {
@@ -7,13 +8,11 @@ const manifest = {
     name: 'Películas Dropbox',
     description: 'Addon privado para ver tus películas en Dropbox',
     types: ['movie'],
-    catalogs: [
-        {
-            type: 'movie',
-            id: 'dropbox',
-            name: 'Películas Dropbox'
-        }
-    ],
+    catalogs: [{
+        type: 'movie',
+        id: 'dropbox',
+        name: 'Películas Dropbox'
+    }],
     resources: ['catalog', 'stream'],
     idPrefixes: ['dropbox:'],
     logo: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Dropbox_Icon.svg',
@@ -22,13 +21,11 @@ const manifest = {
 
 const builder = new addonBuilder(manifest);
 
-const movies = [
-    {
-        id: 'dropbox:untildawn2025',
-        name: 'Until Dawn (2025)',
-        poster: 'https://m.media-amazon.com/images/I/71x6UArEeaL._AC_SL1111_.jpg'
-    }
-];
+const movies = [{
+    id: 'dropbox:untildawn2025',
+    name: 'Until Dawn (2025)',
+    poster: 'https://m.media-amazon.com/images/I/71x6UArEeaL._AC_SL1111_.jpg'
+}];
 
 builder.defineCatalogHandler(() => {
     return Promise.resolve({ metas: movies });
@@ -46,4 +43,18 @@ builder.defineStreamHandler(({ id }) => {
     return Promise.resolve({ streams: [] });
 });
 
-module.exports = builder.getInterface();
+// ✅ Aquí tu addon escucha peticiones desde Stremio
+const app = express();
+const port = process.env.PORT || 7000;
+
+app.get('/manifest.json', (req, res) => {
+    res.send(builder.getInterface().manifest);
+});
+
+app.get('/:resource/:type/:id/:extra?.json', (req, res) => {
+    builder.getInterface().get(req, res);
+});
+
+app.listen(port, () => {
+    console.log(`Addon funcionando en http://localhost:${port}`);
+});
